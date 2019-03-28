e5591db14bb8b04acb4f5887b9f62ed4b040f6e5
Michelle Hoelzer <hoelzermm@gmail.com>
2019-03-28 12:18:23 -0400

    fixed main() and logger and exit_flag issue; fixing other issues tba

--- dirwatcher.py
+++ dirwatcher.py
@@ -10,6 +10,8 @@ import logging
 import signal
 import sys
 import os
+
+
 exit_flag = False
 
 
@@ -22,32 +24,41 @@ def create_logger():
         datefmt="%Y-%m-%d, %H:%M:%S")
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
+
     stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
     return logger
 
 
+logger = create_logger()  # this makes it a global var
+
+
 def start_logger(logger, start_time):
     logger.info(
-        "-------------------------------------------------------------------"
-        + "Running {}".format(__file__)
-        + "Started on {}".format(start_time.isoformat())
-        + "-------------------------------------------------------------------"
+        "\n"
+        "-------------------------------------------------------------------\n"
+        "Running {}\n"
+        "Started on {}\n"
+        "-------------------------------------------------------------------\n"
+        .format(__file__, start_time.isoformat())
     )
 
 
 def stop_logger(logger, start_time):
     uptime = datetime.datetime.now() - start_time
     logger.info(
-        "-------------------------------------------------------------------"
-        + "Stopped {}".format(__file__)
-        + "Uptime was {}".format(str(uptime))
-        + "-------------------------------------------------------------------"
+        "\n"
+        "-------------------------------------------------------------------\n"
+        "Stopped {}\n"
+        "Uptime was {}\n"
+        "-------------------------------------------------------------------\n"
+        .format(__file__, str(uptime))
     )
 
 
-def signal_handler(sig_num, frame, logger):
+def signal_handler(sig_num, frame):
     """
     This is a handler for SIGTERM and SIGINT. Other signals can be mapped here as well (SIGHUP?)
     Basically it just sets a global flag, and main() will exit it's loop if the signal is trapped.
@@ -56,17 +67,18 @@ def signal_handler(sig_num, frame, logger):
     :return None
     """
     # log the associated signal name (the python3 way)
-    logger.warning("Received {}".format(signal.Signals(sig_num).name))
+    # logger.warning("Received {}".format(signal.Signals(sig_num).name))
     # log the signal name (the python2 way)
     signames = dict((k, v)
                     for v, k in reversed(sorted(signal.__dict__.items()))
                     if v.startswith("SIG") and not v.startswith("SIG_"))
     logger.warning("Received {}".format(signames[sig_num]))
+    global exit_flag  # need to specify this as global in order to be used here
     exit_flag = True
 
 
 def watch_directory(args, logger, dir_dict):
-    interval = args.interval
+    # interval = args.interval
     directory = args.directory
     magic = args.magic
     extension = args.extension
@@ -75,7 +87,7 @@ def watch_directory(args, logger, dir_dict):
     # while True:
     #     try:
     #         logger.info("Inside watch loop")
-    #         time.sleep(interval)
+    #         time.sleep(float(interval))
     #     except KeyboardInterrupt:
     #         break
     for file in files:
@@ -84,20 +96,21 @@ def watch_directory(args, logger, dir_dict):
             logger.info("New file added: {}".format(file))
         full_path = os.path.join(directory, file)
         with open(full_path, "r") as read_opened_file:
-            for line, index in enumerate(read_opened_file, 1):
-                if index < dir_dict[file]:
-                    dir_dict[file] = index
-                    if magic in line:
+            for counter, value in enumerate(read_opened_file, 1):
+                if counter > dir_dict[file]:
+                    dir_dict[file] = counter
+                    if magic in value:
                         logger.info('"{}" found in "{}" on line {}'.format(
-                            magic, file, index))
+                            magic, file, counter))
     for key, value in dir_dict:
         if key not in files:
             logger.info(
                 '"{0}" has left the building (a.k.a.: "{0}" was deleted)'.format(key))
             removed_files.append(key)
             dir_dict.pop(key)
-            dir_dict.pop(value)
-    time.sleep(interval)
+    for file in removed_files:
+        dir_dict.pop(value)
+    # time.sleep(float(interval))
 
 
 def create_parser():
@@ -114,17 +127,18 @@ def create_parser():
 
 
 def main(args):
-    logger = create_logger()
+    # global logger
+    # logger = create_logger()
     parser = create_parser()
     if not args:
         parser.print_usage()
         sys.exit(1)
     args = parser.parse_args(args)
     start_time = datetime.datetime.now()
-    print(args)
+    # print(args)
+    start_logger(logger, start_time)
     logger.info('Watching "{}" directory with ".{}" extensions for "{}" every {} seconds'.format(
         args.directory, args.extension, args.magic, args.interval))
-    start_logger(logger, start_time)
     dir_dict = {}
     # Hook these two signals from the OS ..
     signal.signal(signal.SIGINT, signal_handler)
@@ -148,6 +162,7 @@ def main(args):
 
 
 if __name__ == "__main__":
+    # global logger
     # example of cmdln: python dirwatcher.py
     main(sys.argv[1:])
     # main(sys.argv[1:], sys.argv[0])
